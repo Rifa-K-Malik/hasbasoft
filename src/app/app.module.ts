a8f866538b68a17a4280d04b1d46ea2c4b425b83
Rifa-K-Malik <malikrif07@gmail.com>
2020-11-14 13:09:26 +0530

    Updated : added reactive form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
 import { AddTravellerComponent } from './components/add-traveller/add-traveller.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -15,6 +16,7 @@ import { AddTravellerComponent } from './components/add-traveller/add-traveller.
   ],
   imports: [
     BrowserModule,
+    ReactiveFormsModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule
--- src/app/components/add-traveller/add-traveller.component.ts
+++ src/app/components/add-traveller/add-traveller.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 interface Nation {
   value: string;
   viewValue: string;
@@ -48,6 +49,9 @@ interface PYear {
 export class AddTravellerComponent implements OnInit {
   title = 'hasbap';
   constructor() { }
+
+  myReactiveForm:FormGroup
+
   nationality: Nation[] = [
     {value: 'Afghan-0', viewValue: 'Afghan'},
     {value: 'Argentinian-1', viewValue: 'Argentinian'},
@@ -74,20 +78,20 @@ export class AddTravellerComponent implements OnInit {
     {value: 'France-5', viewValue: 'France'},
     {value: 'Greece-6', viewValue: 'Greece'},
     {value: 'India-7', viewValue: 'India'}
-  ]
+  ];
   date: Date[] = [
-    {value: 1-0, viewValue: 1},
-    {value: 2-1, viewValue: 2},
-    {value: 3-2, viewValue: 3},
-    {value: 4-0, viewValue: 4},
-    {value: 5-1, viewValue: 5},
-    {value: 6-2, viewValue: 6},
-    {value: 7-0, viewValue: 7},
-    {value: 8-1, viewValue: 8},
-    {value: 9-2, viewValue: 9},
-    {value: 10-0, viewValue: 10},
-    {value: 11-1, viewValue: 11},
-    {value: 12-2, viewValue: 12}
+    {value: 1, viewValue: 1},
+    {value: 2, viewValue: 2},
+    {value: 3, viewValue: 3},
+    {value: 4, viewValue: 4},
+    {value: 5, viewValue: 5},
+    {value: 6, viewValue: 6},
+    {value: 7, viewValue: 7},
+    {value: 8, viewValue: 8},
+    {value: 9, viewValue: 9},
+    {value: 10, viewValue: 10},
+    {value: 11, viewValue: 11},
+    {value: 12, viewValue: 12}
   ];
 
   month: Month[] = [
@@ -106,29 +110,34 @@ export class AddTravellerComponent implements OnInit {
   ];
 
   year: Year[] = [
-    {value: 1990-0, viewValue: 1990},
-    {value: 1991-1, viewValue: 1991},
-    {value: 1992-2, viewValue: 1992},
-    {value: 1993-0, viewValue: 1993},
-    {value: 1994-1, viewValue: 1994},
-    {value: 1995-2, viewValue: 1995},
-    {value: 1996-0, viewValue: 1996},
-    {value: 1997-1, viewValue: 1997},
-    {value: 1998-2, viewValue: 1998}
-  ]
+    {value: 1990, viewValue: 1990},
+    {value: 1991, viewValue: 1991},
+    {value: 1992, viewValue: 1992},
+    {value: 1993, viewValue: 1993},
+    {value: 1994, viewValue: 1994},
+    {value: 1995, viewValue: 1995},
+    {value: 1996, viewValue: 1996},
+    {value: 1997, viewValue: 1997},
+    {value: 1998, viewValue: 1998},
+    {value: 1999, viewValue: 1999},
+    {value: 2000, viewValue: 2000},
+    {value: 2001, viewValue: 2001},
+    {value: 2002, viewValue: 2002},
+    {value: 2003, viewValue: 2003}
+  ];
   Pdate: Date[] = [
-    {value: 1-0, viewValue: 1},
-    {value: 2-1, viewValue: 2},
-    {value: 3-2, viewValue: 3},
-    {value: 4-0, viewValue: 4},
-    {value: 5-1, viewValue: 5},
-    {value: 6-2, viewValue: 6},
-    {value: 7-0, viewValue: 7},
-    {value: 8-1, viewValue: 8},
-    {value: 9-2, viewValue: 9},
-    {value: 10-0, viewValue: 10},
-    {value: 11-1, viewValue: 11},
-    {value: 12-2, viewValue: 12}
+    {value: 1, viewValue: 1},
+    {value: 2, viewValue: 2},
+    {value: 3, viewValue: 3},
+    {value: 4, viewValue: 4},
+    {value: 5, viewValue: 5},
+    {value: 6, viewValue: 6},
+    {value: 7, viewValue: 7},
+    {value: 8, viewValue: 8},
+    {value: 9, viewValue: 9},
+    {value: 10, viewValue: 10},
+    {value: 11, viewValue: 11},
+    {value: 12, viewValue: 12}
   ];
 
   Pmonth: Month[] = [
@@ -147,18 +156,43 @@ export class AddTravellerComponent implements OnInit {
   ];
 
   Pyear: Year[] = [
-    {value: 1990-0, viewValue: 1990},
-    {value: 1991-1, viewValue: 1991},
-    {value: 1992-2, viewValue: 1992},
-    {value: 1993-0, viewValue: 1993},
-    {value: 1994-1, viewValue: 1994},
-    {value: 1995-2, viewValue: 1995},
-    {value: 1996-0, viewValue: 1996},
-    {value: 1997-1, viewValue: 1997},
-    {value: 1998-2, viewValue: 1998}
-  ]
+    {value: 1990, viewValue: 1990},
+    {value: 1991, viewValue: 1991},
+    {value: 1992, viewValue: 1992},
+    {value: 1993, viewValue: 1993},
+    {value: 1994, viewValue: 1994},
+    {value: 1995, viewValue: 1995},
+    {value: 1996, viewValue: 1996},
+    {value: 1997, viewValue: 1997},
+    {value: 1998, viewValue: 1998},
+    {value: 1999, viewValue: 1999},
+    {value: 2000, viewValue: 2000},
+    {value: 2001, viewValue: 2001},
+    {value: 2002, viewValue: 2002},
+    {value: 2003, viewValue: 2003}
+  ];
+ 
+  ngOnInit() {
+    this.myReactiveForm = new FormGroup({
+      'firstname': new FormControl(''),
+      'lastname': new FormControl(''),
+      'dob date': new FormControl(''),
+      'dob month': new FormControl(''),
+      'dob year': new FormControl(''),
+      'gender': new FormControl(''),
+      'nationality': new FormControl(''),
+      'passportno': new FormControl(''),
+      'pexpirydate date': new FormControl(''),
+      'pexpirydate month': new FormControl(''),
+      'pexpirydate year': new FormControl(''),
+      'pissuancecountry': new FormControl('')
 
-  ngOnInit(): void {
+    });
+  }
+
+  onSubmit(){
+    console.log(this.myReactiveForm.value);
   }
 
 }
+ 
